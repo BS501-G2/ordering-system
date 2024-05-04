8634291779da11234d68c98176108246af6c7e4a
Rizzzi Git <rizzzigit@gmail.com>
2024-05-05 07:35:44 +0800

    Sun May  5 07:35:44 AM PST 2024

---
+++ src/routes/+page.svelte
@@ -0,0 +1,223 @@
+<div class="pageContainer" style="display: flex;justify-content:space-between; background-color: #ebf4e4;">
+    <div class="card1" style="background-color: white; height:980px; width:1400px; margin:20px; border-radius:25px; ">
+        <div style="display:flex; justify-content:center;margin-top:20px" >
+            <div class="firstMenu" style="background-color:#ebf4e4; margin:20px; height:430px; width:300px;border-radius:20px" >
+                <div>
+                    <img src="https://d1r3vc4fck3z1b.cloudfront.net/images/1707814197_web_variance_00DwO6iL.jpeg" alt="" srcset="" style="width:300px; height:250px;border-top-left-radius: 20px; border-top-right-radius:20px">
+                </div>
+                <div style="margin:10px; margin-top:-15px;margin-bottom:-15px">
+                    <p style="font-weight:bold; font-size:20px">
+                        Crispy Fish Fillet Sandwich w/ Fries & PeaChee Fruit Fizz Meal
+                    </p>
+                </div>
+                <div>
+                    <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+                </div>
+                <div style="margin-top:-15px; margin-left:20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        P 275.00
+                    </p>
+                </div>
+                <div style="display:flex; justify-content:center; margin-top:-15px">
+                    <button style="padding:10px 70px; border-radius:20px; cursor:pointer; background-color:#37812e; font-weight:bold; font-size:14px; color:white;transition: background-color 0.3s;">Order</button>
+                </div>
+            </div>
+    
+            <div class="Menu2" style="background-color:#ebf4e4; margin:20px; height:430px; width:300px;border-radius:20px" >
+                <div>
+                    <img src="https://d1r3vc4fck3z1b.cloudfront.net/images/1707815782_web_variance_5NIkc8HG.jpeg" alt="" srcset="" style="width:300px; height:250px;border-top-left-radius: 20px; border-top-right-radius:20px">
+                </div>
+                <div style="margin:10px; margin-top:-15px;margin-bottom:-15px">
+                    <p style="font-weight:bold; font-size:20px">
+                        Double Crispy Fish Fillet Sandwich w/ Fries & PeaChee Fruit Fizz Meal
+                    </p>
+                </div>
+                <div>
+                    <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+                </div>
+                <div style="margin-top:-15px; margin-left:20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        P 388.00
+                    </p>
+                </div>
+                <div style="display:flex; justify-content:center; margin-top:-15px">
+                    <button style="padding:10px 70px; border-radius:20px; cursor:pointer; background-color:#37812e; font-weight:bold; font-size:14px; color:white">Order</button>
+                </div>
+            </div>
+    
+            <div class="Menu3" style="background-color:#ebf4e4; margin:20px; height:430px; width:300px;border-radius:20px" >
+                <div>
+                    <img src="https://d1r3vc4fck3z1b.cloudfront.net/images/1698314359_web_variance_pajShh4d.jpeg" alt="" srcset="" style="width:300px; height:250px;border-top-left-radius: 20px; border-top-right-radius:20px">
+                </div>
+                <div style="margin:10px; margin-top:-15px;margin-bottom:-20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        1-pc. Chicken McDo w/ Double Rice Small Meal  
+                    </p>
+                    <br>
+                </div>
+                <div>
+                    <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+                </div>
+                <div style="margin-top:-15px; margin-left:20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        P 155.00
+                    </p>
+                </div>
+                <div style="display:flex; justify-content:center; margin-top:-15px">
+                    <button style="padding:10px 70px; border-radius:20px; cursor:pointer; background-color:#37812e; font-weight:bold; font-size:14px; color:white">Order</button>
+                </div>
+            </div>
+    
+            <div class="Menu4" style="background-color:#ebf4e4; margin:20px; height:430px; width:300px;border-radius:20px" >
+                <div>
+                    <img src="https://d1r3vc4fck3z1b.cloudfront.net/images/1698333158_web_alacarte_4xRv9inE.jpeg" alt="" srcset="" style="width:300px; height:250px;border-top-left-radius: 20px; border-top-right-radius:20px">
+                </div>
+                <div style="margin:10px; margin-top:-15px;margin-bottom:-20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        1-pc. Chicken McDo w/ Rice & Coke McFloat Meal
+                    </p>
+                    <br>
+                </div>
+                <div>
+                    <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+                </div>
+                <div style="margin-top:-15px; margin-left:20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        P 156.00
+                    </p>
+                </div>
+                <div style="display:flex; justify-content:center; margin-top:-15px">
+                    <button style="padding:10px 70px; border-radius:20px; cursor:pointer; background-color:#37812e; font-weight:bold; font-size:14px; color:white">Order</button>
+                </div>
+            </div>
+            
+        </div>
+        <div style="display:flex; justify-content:center;" >
+            <div class="Menu5" style="background-color:#ebf4e4; margin:20px; height:430px; width:300px;border-radius:20px" >
+                <div>
+                    <img src="https://d1r3vc4fck3z1b.cloudfront.net/images/1633576845_mobile_variace_cacKRwNX.jpeg" alt="" srcset="" style="width:300px; height:250px;border-top-left-radius: 20px; border-top-right-radius:20px">
+                </div>
+                <div style="margin:10px; margin-top:-15px;margin-bottom:-15px">
+                    <p style="font-weight:bold; font-size:20px">
+                        McShare Bundle for 3
+                    </p>
+                    <br> 
+                    <br>
+                </div>
+                <div>
+                    <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+                </div>
+                <div style="margin-top:-15px; margin-left:20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        P 612.00
+                    </p>
+                </div>
+                <div style="display:flex; justify-content:center; margin-top:-15px">
+                    <button style="padding:10px 70px; border-radius:20px; cursor:pointer; background-color:#37812e; font-weight:bold; font-size:14px; color:white">Order</button>
+                </div>
+            </div>
+    
+            <div class="Menu6" style="background-color:#ebf4e4; margin:20px; height:430px; width:300px;border-radius:20px" >
+                <div>
+                    <img src="https://d1r3vc4fck3z1b.cloudfront.net/images/1634002555_mobile_variace_mFMhp04J.jpeg" alt="" srcset="" style="width:300px; height:250px;border-top-left-radius: 20px; border-top-right-radius:20px">
+                </div>
+                <div style="margin:10px; margin-top:-15px;margin-bottom:-15px">
+                    <p style="font-weight:bold; font-size:20px">
+                        McShare Bundle for 4
+                    </p>
+                    <br> 
+                    <br>
+                </div>
+                <div>
+                    <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+                </div>
+                <div style="margin-top:-15px; margin-left:20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        P 842.00
+                    </p>
+                </div>
+                <div style="display:flex; justify-content:center; margin-top:-15px">
+                    <button style="padding:10px 70px; border-radius:20px; cursor:pointer; background-color:#37812e; font-weight:bold; font-size:14px; color:white">Order</button>
+                </div>
+            </div>
+    
+            <div class="Menu7" style="background-color:#ebf4e4; margin:20px; height:430px; width:300px;border-radius:20px" >
+                <div>
+                    <img src="https://d1r3vc4fck3z1b.cloudfront.net/images/1707814197_web_variance_00DwO6iL.jpeg" alt="" srcset="" style="width:300px; height:250px;border-top-left-radius: 20px; border-top-right-radius:20px">
+                </div>
+                <div style="margin:10px; margin-top:-15px;margin-bottom:-15px">
+                    <p style="font-weight:bold; font-size:20px">
+                        Crispy Fish Fillet Sandwich w/ Fries & PeaChee Fruit Fizz Meal
+                    </p>
+                </div>
+                <div>
+                    <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+                </div>
+                <div style="margin-top:-15px; margin-left:20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        P 275.00
+                    </p>
+                </div>
+                <div style="display:flex; justify-content:center; margin-top:-15px">
+                    <button style="padding:10px 70px; border-radius:20px; cursor:pointer; background-color:#37812e; font-weight:bold; font-size:14px; color:white">Order</button>
+                </div>
+            </div>
+    
+            <div class="firstMenu" style="background-color:#ebf4e4; margin:20px; height:430px; width:300px;border-radius:20px" >
+                <div>
+                    <img src="https://d1r3vc4fck3z1b.cloudfront.net/images/1707814197_web_variance_00DwO6iL.jpeg" alt="" srcset="" style="width:300px; height:250px;border-top-left-radius: 20px; border-top-right-radius:20px">
+                </div>
+                <div style="margin:10px; margin-top:-15px;margin-bottom:-15px">
+                    <p style="font-weight:bold; font-size:20px">
+                        Crispy Fish Fillet Sandwich w/ Fries & PeaChee Fruit Fizz Meal
+                    </p>
+                </div>
+                <div>
+                    <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+                </div>
+                <div style="margin-top:-15px; margin-left:20px">
+                    <p style="font-weight:bold; font-size:20px">
+                        P 275.00
+                    </p>
+                </div>
+                <div style="display:flex; justify-content:center; margin-top:-15px">
+                    <button style="padding:10px 70px; border-radius:20px; cursor:pointer; background-color:#37812e; font-weight:bold; font-size:14px; color:white">Order</button>
+                </div>
+            </div>
+            
+        </div>
+       
+        
+
+    </div>
+   
+
+    
+
+
+    <div class="myBag" style="background-color: white; height:980px; width:430px; margin:20px; border-radius:25px">
+        <div class="title" style="display:flex; justify-content:center; padding-top:10px; margin-bottom:-20px;">
+            <h2 style="font-weight:400px; font-size: 38px">My Bag</h2>
+        </div>
+        <div>
+            <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+        </div>
+        <div style="margin-top:730px; margin-bottom:-10px">
+            <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+        </div>
+        <div style="display: flex; justify-content:space-between; margin-left:30px; margin-right:30px; font-size:18px; font-weight: bold;">
+            <p>Total</p>
+            <p>P 0.00</p>
+        </div>
+       
+        <div style="margin-top:-10px">
+            <hr style="width: 90%; border: none; border-top: 1px solid #37812e;">
+        </div>
+        <div style="display:flex; justify-content:center; margin:10px; padding-top:10px">
+            <button style="padding:15px 100px; border-radius: 20px; cursor:pointer; background-color:#37812e; color:white; font-weight:bold; font-size:14px;">Proceed to checkout</button>
+        </div>
+
+    </div>
+
+</div>
+
